342dcf8060b41b3e15109324e0ccdf8d2c89d3b5
Eric Litak <elitak@gmail.com>
2014-07-23 04:30:14 -0700

    things to try

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,9 @@ import Shelly
 import Prelude hiding (putStrLn)
 import Data.Text.IO
 
+-- TODO windows shell commands? detect os and run the appropriate commands?
+-- TODO cross-comp to windows exe?
+
 main = do
     listing <- shelly $ silently $ do
         run_ "echo" ["hello", "world"]
